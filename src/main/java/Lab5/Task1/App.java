76b297d9ef96f61d27745b2596957463a5d49720
vihoviha <xakayaduv@gmail.com>
2021-04-07 18:42:18 +0300

    Task0501 Done

--- src/main/java/Lab5/Task1/App.java
+++ src/main/java/Lab5/Task1/App.java
@@ -1,4 +1,23 @@
 package Lab5.Task1;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("../../main.fxml"));
+        stage.setTitle("Проста форма");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/main/java/Lab5/Task1/controller/MainController.java
@@ -0,0 +1,9 @@
+package Lab5.Task1.controller;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+    public void pressedButton(ActionEvent actionEvent) {
+        System.out.println("Button was pressed");
+    }
+}
